3ebd7bd1689aea65a3c24a201ce7bd7de10e01ce
Gediminas Dackys <gdackys@gmail.com>
2011-12-08 09:54:21 +0000

    Add start event

--- file_upload.js
+++ file_upload.js
@@ -1,10 +1,16 @@
 (function($) {
   $.fn.file_upload = function(url, options) {
     var settings = $.extend({
+      start:    $.noop,
       success:  $.noop,
       fail:     $.noop,
       progress: $.noop
     }, options);
+
+    var _start = function(event) {
+      var xhr = event.target;
+      settings.start.apply(this, [xhr]);
+    }
 
     var _load = function(event) {
       var xhr = event.target;
@@ -27,6 +33,7 @@
       form_data.append(this.id, this.files[0]);
       xhr.upload.addEventListener('progress', $.proxy(_progress, this), false);
       xhr.addEventListener('load', $.proxy(_load, this), false);
+      xhr.addEventListener('loadstart', $.proxy(_start, this), false);
 
       xhr.open('POST', url, true);
       xhr.send(form_data);
